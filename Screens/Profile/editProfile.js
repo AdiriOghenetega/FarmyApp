72a93c0d44862e7242181480199b4bfd09cb3a69
AdiriOghenetega <tegaadiri3@yahoo.com>
2023-10-25 18:29:09 +0100

    updated profile edit feature

--- Screens/Profile/editProfile.js
+++ Screens/Profile/editProfile.js
@@ -5,52 +5,101 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  Modal,
+  ImageBackground,
 } from "react-native";
 import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import colors from "../../configs/colors";
 import mcd from "../../assets/images/mcd.jpg";
-import EditProfilePic from "./editProfilePic";
 import GlassmorphicInput from "../../configs/GlassmorphicInput";
 import CustomButton from "../../configs/CustomButton";
 import CustomHeader from "../../configs/customHeader";
+import * as ImagePicker from "expo-image-picker";
 
 export default function EditProfile({ navigation }) {
-  const [editModal, setEditModal] = useState(false);
   const [editData, setEditData] = useState({
     username: "",
     name: "",
     phoneNumber: "",
     email: "",
+    image: "",
+    coverImage: "",
   });
 
-  const handleEditModal = () => {
-    setEditModal((prev) => !prev);
+  const pickImage = async (id) => {
+    try {
+      // No permissions request is necessary for launching the image library
+
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+
+      if (status === "granted") {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 0.5,
+        });
+
+        if (!result.canceled) {
+          setEditData((prev) => {
+            return {
+              ...prev,
+              [id]: result.assets[0].uri,
+            };
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error, "unable to upload image");
+      toast("Unable to upload image,try again");
+    }
   };
 
-  const handleUpdateProfile=()=>{
-    console.log("updated")
-  }
+  const handleUpdateProfile = () => {
+    console.log("updated");
+  };
 
-  const handleNavigate = ()=>{
-    navigation.navigate("Profile")
-  }
+  const handleNavigate = () => {
+    navigation.navigate("Profile");
+  };
 
   return (
     <SafeAreaView>
-      <Modal visible={editModal}>
-        <EditProfilePic handleEditModal={handleEditModal} />
-      </Modal>
-      <CustomHeader title={"Edit Profile"} handleNavigate={handleNavigate}  />
-      <View style={styles.profilePicParent}>
+      <CustomHeader title={"Edit Profile"} handleNavigate={handleNavigate} />
+      <ImageBackground
+        source={{
+          uri: editData.coverImage
+            ? editData.coverImage
+            : "https://plus.unsplash.com/premium_photo-1678344170545-c3edef92a16e?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZmFybXxlbnwwfHwwfHx8MA%3D%3D",
+        }}
+        style={styles.profilePicParent}
+      >
         <View style={styles.profilePicContainer}>
-          <Image source={mcd} alt="profilePic" style={styles.profilePic} />
+          <Image
+            source={editData.image ? { uri: editData.image } : mcd}
+            alt="profilePic"
+            style={styles.profilePic}
+          />
+          <TouchableOpacity
+            style={styles.editPic}
+            onPress={() => pickImage("image")}
+          >
+            <MaterialIcons name="edit" size={24} color={colors.white} />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.editPic} onPress={handleEditModal}>
+        <TouchableOpacity
+          style={styles.editPic2}
+          onPress={() => pickImage("coverImage")}
+        >
           <MaterialIcons name="edit" size={24} color={colors.white} />
         </TouchableOpacity>
-      </View>
+      </ImageBackground>
+
       <View style={styles.formContainer}>
         <View style={styles.textInputContainer}>
           <GlassmorphicInput
@@ -132,8 +181,15 @@ const styles = StyleSheet.create({
   },
   editPic: {
     position: "absolute",
-    height: 42,
-    width: 80,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    bottom: 10,
+    right: 10,
+  },
+  editPic2: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
     alignItems: "flex-end",
     justifyContent: "flex-end",
   },
@@ -141,9 +197,9 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
   },
-  formContainer:{
-    height:250,
-    justifyContent:"space-between",
-    marginVertical:10
-  }
+  formContainer: {
+    height: 250,
+    justifyContent: "space-between",
+    marginVertical: 20,
+  },
 });
